c09b6e0de7dc1f881a065287d4378ecbd284be82
ssnghyxn-k <kimsunghyun8312@gmail.com>
2024-09-30 18:34:56 +0900

    Drill #5 제출 (2020180007 김성현)
    still on work

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -1,8 +1,8 @@
 from pico2d import *
 
-open_canvas()
+open_canvas(800,600)
 tukorea = load_image('TUK_GROUND.png')
-character = load_image('character_sheet.png')
+character = load_image('character_sheet_2.png')
 
 def handle_events():
     global move, dir_x, dir_y
@@ -34,20 +34,38 @@ def handle_events():
 
 move = True
 x = 800 // 2
-y = 90
+y = 600 // 2
 frame = 0
 dir_x = 0
 dir_y = 0
+w = 800
+h = 600
+frame_w = 75
+frame_h = 75
 
 while move:
     clear_canvas()
     tukorea.draw(500,90)
-    character.clip_draw(frame * 100, 100, 100, 100, x, y)
+    frame_x = (frame % 3) * frame_w
+    frame_y = (3 - (frame // 3) - 1) * frame_h
+    character.clip_draw(frame_x, frame_y, frame_w, frame_h, x, y)
     update_canvas()
     handle_events()
-    frame = (frame + 1) % 4
-    x += dir_x * 20
-    y += dir_y * 20
+
+    frame = (frame + 1) % 12
+    x += dir_x * 10
+    y += dir_y * 10
+
+    if x < 0:
+        x = 0
+    elif x > w:
+        x = w - 100
+
+    if y < 0:
+        y = 0
+    elif y > h:
+        y = h - 100
+
     delay (0.05)
 
 close_canvas()
