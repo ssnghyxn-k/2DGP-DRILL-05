2df548d73eae077401dea3f4186ed20b61998fbb
ssnghyxn-k <kimsunghyun8312@gmail.com>
2024-09-30 18:05:37 +0900

    Add move

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -32,6 +32,22 @@ def handle_events():
             elif event.key == SDLK_DOWN:
                 dir_y += 1
 
+move = True
+x = 800 // 2
+y = 90
+frame = 0
+dir_x = 0
+dir_y = 0
 
+while move:
+    clear_canvas()
+    tukorea.draw(500,90)
+    character.clip_draw(frame * 100, 100, 100, 100, x, y)
+    update_canvas()
+    handle_events()
+    frame = (frame + 1) % 4
+    x += dir_x * 20
+    y += dir_y * 20
+    delay (0.05)
 
 close_canvas()
